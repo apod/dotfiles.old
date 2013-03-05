3fff5cffd2a86ef1c6312822cd51e25dfa87c125
Alex Podaras <apodaras@gmail.com>
2013-03-05 13:16:32 +0200

    Git aliases

---
+++ zsh/oh-my-zsh/plugins/git/git.plugin.zsh
@@ -0,0 +1,43 @@
+# Aliases
+alias g='git'
+compdef g=git
+alias gst='git status'
+compdef _git gst=git-status
+alias gl='git pull'
+compdef _git gl=git-pull
+alias gp='git push'
+compdef _git gp=git-push
+alias gc='git commit'
+compdef _git gc=git-commit
+alias gcv='git commit -v'
+compdef _git gc=git-commit
+alias gca='git commit -v -a'
+compdef _git gca=git-commit
+alias gb='git branch'
+compdef _git gb=git-branch
+alias gcount='git shortlog -sn'
+compdef gcount=git
+alias ga='git add'
+compdef _git ga=git-add
+alias gm='git merge'
+compdef _git gm=git-merge
+alias grh='git reset HEAD'
+alias gf='git ls-files | grep'
+
+# Will cd into the top of the current repository
+# or submodule.
+alias grt='cd $(git rev-parse --show-toplevel || echo ".")'
+
+# Will return the current branch name
+# Usage example: git pull origin $(current_branch)
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+function current_repository() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo $(git remote -v | cut -d':' -f 2)
+}
