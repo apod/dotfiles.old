0b16591259df1f672bf8b316b0ac07cea6a5f391
Alex Podaras <apodaras@gmail.com>
2014-03-01 12:18:37 +0200

    Add ctrl-p/ctrl-n keybindings for history search

--- zsh/oh-my-zsh/02_keybindings.zsh
+++ zsh/oh-my-zsh/02_keybindings.zsh
@@ -1,5 +1,7 @@
 bindkey "\ek" kill-whole-line # Esc-k delete the current line
 
-# History search with up and down arrow keys
+# Search history with up/ctrl-p and down/ctrl-n arrow keys
 bindkey '^[[A' history-beginning-search-backward
 bindkey '^[[B' history-beginning-search-forward
+bindkey '^P' history-beginning-search-backward
+bindkey '^N' history-beginning-search-forward
